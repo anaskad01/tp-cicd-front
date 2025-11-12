b1d4cd51e4ee3e496eea64ce36a5f4f343e173cf
anaskad01 <anaskadar.01@icloud.com>
2025-11-12 16:51:46 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+
+const plugins = [vue()]
+
+if (process.env.NODE_ENV !== 'production') {
+  const vueDevTools = require('vite-plugin-vue-devtools')
+  plugins.push(vueDevTools())
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
